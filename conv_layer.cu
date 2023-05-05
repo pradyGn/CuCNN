87527ce4ef256ee3e4f6daeb0bea6803e797b893
pradyG <pradyumangangan.pag@gmail.com>
2023-05-05 19:18:53 -0400

    conv layer test

---
+++ conv_layer.cu
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <iostream>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define input_M 3
+#define filter_M 2
+#define output_M 4
+
+__global__ void convolutional_layer2D(float *filter, int filter_M, int filter_N, float *input, int input_M, int input_N, float *output, int output_M, int output_N)
+{
+    int i = threadIdx.x;
+    int j = blockIdx.x;
+
+    for (int m = 0; m < filter_M; m++){
+        for (int n = 0; n < filter_N, n++){
+            int output_pos = (i + (filter_N - 1) - n) + (j + (filter_M - 1) - m) * output_N;
+            int input_pos = i + (j*input_N);
+            int filter_pos = m + (n*filter_N);
+            output[output_pos] += input[input_pos] * filter[filter_pos];
+        }
+    }
+
+
+}
+
+void initialize(float *matrix, int matrix_M, int matrix_N){
+    for (int i = 0; i < matrix_M; i++){
+        for (int j = 0; j < matrix_N; j++){
+            matrix[(i*matrix_N) + j] = j + i;
+        }
+    }
+}
+
+
+int main(){
+
+    //int input_M = 3;
+    //int filter_M = 2;
+    //int output_M = input_M + 1;
+
+    float *d_output, *h_output, *d_filter, *h_filter, *d_input, *h_input;
+
+    h_output = (float*)malloc(sizeof(float) * (output_M * output_M));
+    h_filter = (float*)malloc(sizeof(float) * (filter_M * filter_M));
+    h_input = (float*)malloc(sizeof(float) * (input_M * input_M));
+
+    initialize(h_filter, filter_M, filter_M)
+    initialize(h_input, input_M, input_M)
+
+    cudaMalloc((void**)&d_output, sizeof(float) * (output_M * output_M));
+    cudaMalloc((void**)&d_filter, sizeof(float) * (filter_M * filter_M));
+    cudaMalloc((void**)&d_input, sizeof(float) * (input_M * input_M));
+
+    cudaMemcpy(d_filter, h_filter, sizeof(float) * (filter_M * filter_M), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_input, h_input, sizeof(float) * (input_M * input_M), cudaMemcpyHostToDevice);
+
+
+    dim3 gridsize(input_M);
+    dim3 blocksize(input_M);
+
+    convolutional_layer2D <<<gridsize, blocksize>>>(d_filter, filter_M, filter_M, d_input, input_M, input_M, d_output, output_M, output_M);
+
+    cudaMemcpy(h_output, d_output, sizeof(float) * (output_M * output_M), cudaMemcpyDeviceToHost);
+
+    for (int i=0; i<output_M; i++){
+        for (int j=0; j<output_M; j++)
+        {
+                printf("%.2f", h_output[(i*matrix_N)+j]);
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    
+
+}

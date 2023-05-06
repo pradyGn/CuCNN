2e7a2738bb8b9dc0651ecf4006f2670722fe6d71
pradyG <pradyumangangan.pag@gmail.com>
2023-05-05 22:16:30 -0400

    test

--- conv_layer.cu
+++ conv_layer.cu
@@ -19,20 +19,19 @@ __global__ void convolutional_layer2D (float *filter, float *input, float *outpu
 
     int input_pos = i + (j*input_N);
 
-    output[input_pos] = filter[3] * input[i + j*input_N];
+    //output[input_pos] = filter[3] * input[i + j*input_N];
 
     //int output_pos = 0;
-    /*
+    
     for (int m = 0; m < filter_M; m++){
         for (int n = 0; n < filter_N; n++){
-            output_pos = (i + (filter_N - 1) - n) + (j + (filter_M - 1) - m) * output_N;
+            //int output_pos = (i + (filter_N - 1) - n) + (j + (filter_M - 1) - m) * output_N;
             //int filter_pos = (m*filter_N) + n;
             //output[output_pos] += input[input_pos] * filter[filter_pos];
-            //output[0] += i;
-            output[0] += 1;
+            output[input_pos] += filter[3] * input[i + j*input_N];
         }
     }
-    */
+    
     
     
 

c6a294563bd0e8479a246457b42c02700f5c3bc2
pradyG <pradyumangangan.pag@gmail.com>
2023-05-16 03:00:33 -0400

    backprop comnv

--- activation_fn.h
+++ activation_fn.h
@@ -9,3 +9,17 @@ __global__ void sigmoid_function(float* input, float* output){
     output[i] = 1/(1 + exp(-1*input[i]));
 }
 
+
+__global__ void softmax_denom(float* denom, float* input){
+    int i = threadIdx.x;
+
+    denom[i] += exp(input[i])
+
+}
+
+__global__ void softmax(float* denom, float* input, float* softmax_return){
+    int i = threadIdx.x;
+
+    softmax_return[i] = exp(input[i])/denom[0];
+
+}

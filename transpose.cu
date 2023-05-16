39f625b6aa6ec534ff38aa69fc8296e54c760103
pradyG <pradyumangangan.pag@gmail.com>
2023-05-16 01:21:50 -0400

    transpose test

--- transpose.cu
+++ transpose.cu
@@ -11,7 +11,7 @@ __global__ void transpose(float *matrix_t, float *matrix){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.x;
 
-    matrix_t[i] = matrix[floor(blockDim.x * (i % gridDim.x) + floor(i / gridDim.x))];
+    matrix_t[i] = matrix[(blockDim.x * (i % gridDim.x) + (i / gridDim.x))];
 }
 
 

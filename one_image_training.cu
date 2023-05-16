638d4d5d39188dd6b906bb8bcb01ec5c2959ca01
pradyG <pradyumangangan.pag@gmail.com>
2023-05-16 05:46:31 -0400

    cadsjbvcsidjb

--- one_image_training.cu
+++ one_image_training.cu
@@ -305,7 +305,10 @@ int main(){
             //check_matrix(&h_output[784*i], output_M, output_M);
             //check_matrix(&h_dense_output[10*i], 1, dense_output_M);
             //check_matrix(h_weights,dense_output_M,output_M*output_M);
-            check_matrix(h_dense_grad_input_act, 1, output_M*output_M);
+            //check_matrix(h_dense_grad_input_act, 1, output_M*output_M);
+            for (int j = 0; j < output_M * output_M; j++){
+                cout << h_dense_grad_input_act[j] << endl;
+            }
             cout<<"Hello from 1"<<endl;
         }
 

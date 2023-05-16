2413b18044f341f45a8410f9da9f08ee0d43acc0
pks2902 <pranav2902@deepmm.com>
2023-05-15 20:54:15 -0400

    check

--- one_image_training.cu
+++ one_image_training.cu
@@ -51,10 +51,10 @@ int main(){
     cudaMalloc((void**)&d_bias_dense, sizeof(float) * dense_output_M);
     cudaMemcpy(d_weights, h_weights, sizeof(float) * (dense_output_M * (output_M * output_M)), cudaMemcpyHostToDevice);
     cudaMemcpy(d_bias_dense, h_bias_dense, sizeof(float) * dense_output_M, cudaMemcpyHostToDevice);
-    check_matrix(h_bias_dense, 1, dense_output_M);
-    check_matrix(h_weights, dense_output_M, (output_M * output_M));
+    //check_matrix(h_bias_dense, 1, dense_output_M);
+    //check_matrix(h_weights, dense_output_M, (output_M * output_M));
 
-    for (int i = 0; i < 1; i++){
+    for (int i = 0; i < 2; i++){
 
         initialize_output(&h_output[784*i], output_N, output_N);
         initialize_dense_output(&h_dense_output[10*i]);
